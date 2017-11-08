52e365865b54e65243a7c0c7df1ad63566d36bcb
meeralubis <ameeralubis@gmail.com>
2017-11-08 20:05:00 +0800

    latest

--- js/main.controller.js
+++ js/main.controller.js
@@ -10,11 +10,9 @@
       init = function() {
         vm.data = CompanyData;
         vm.today = new Date();
-        vm.active = 0;
         vm.selectJob = function(job) {
           vm.selectedJob = job
         };
-
       };
 
       init();
